524c6be9bb2fda4964c892ffbbca40991cec63fc
Wen Xiao <wennstyle@gmail.com>
2022-07-21 22:25:57 -0400

    commit tree e318e551be8daefe1fe1dda5a3f3461fbc26771f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
   get '/games' do
     games = Game.all.order(:title).limit(10)
     games.to_json
@@ -15,5 +25,22 @@ class ApplicationController < Sinatra::Base
       } }
     })
   end
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
+
 
 end
